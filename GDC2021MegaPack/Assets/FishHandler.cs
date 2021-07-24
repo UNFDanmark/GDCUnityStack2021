b95569db3b74f6a6694db75c445a2d9b9150d21d
Daxode <dax@daxode.dk>
2021-07-24 08:59:32 +0200

    no message

---
+++ GDC2021MegaPack/Assets/FishHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class FishHandler : MonoBehaviour {
+    [SerializeField] Fishes fishType;
+    [SerializeField] FishesD[] fishTypeD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
---
+++ GDC2021MegaPack/Assets/Fishes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefaultNamespace {
+    [UnityEngine.CreateAssetMenu(fileName = "Fish", menuName = "Submawinee:3/Fish")]
+    public class Fishes : UnityEngine.ScriptableObject {
+        public float health;
+        public float speed;
+    }
+
+    [Serializable]
+    public struct FishesD {
+        public float health;
+        public float speed;
+    }
+}
